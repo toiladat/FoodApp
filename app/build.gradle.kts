a19a99789359476ada6456e1378ae291a467666c
nguyen thanh thuy <nguyenthanhthuy@nguyens-MacBook-Air.local>
2025-06-13 21:24:23 +0700

    Update lại model thêm trường URL vào để lấy được link ảnh

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,5 +45,8 @@ dependencies {
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-database:20.3.0")
     implementation ("com.google.firebase:firebase-auth:22.3.1")
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
+
 
 }
--- app/src/main/java/com/example/foodapp/CartModel.java
+++ app/src/main/java/com/example/foodapp/CartModel.java
@@ -1,29 +1,35 @@
 package com.example.foodapp;
 
 public class CartModel {
+    private int id; // nếu không cần có thể xoá
     private String name;
     private double price;
     private int quantity;
-    private int imageResource;
+    private String imageUrl;
 
     public CartModel() {
         // Firebase cần constructor rỗng
     }
 
-    public CartModel(String name, double price, int quantity, int imageResource) {
+    public CartModel(int id, String name, double price, int quantity, String imageUrl) {
+        this.id = id;
         this.name = name;
         this.price = price;
         this.quantity = quantity;
-        this.imageResource = imageResource;
+        this.imageUrl = imageUrl;
     }
 
+    // Getters
+    public int getId() { return id; }
     public String getName() { return name; }
     public double getPrice() { return price; }
     public int getQuantity() { return quantity; }
-    public int getImageResource() { return imageResource; }
+    public String getImageUrl() { return imageUrl; }
 
+    // Setters
+    public void setId(int id) { this.id = id; }
     public void setName(String name) { this.name = name; }
     public void setPrice(double price) { this.price = price; }
     public void setQuantity(int quantity) { this.quantity = quantity; }
-    public void setImageResource(int imageResource) { this.imageResource = imageResource; }
+    public void setImageUrl(String imageUrl) { this.imageUrl = imageUrl; }
 }
