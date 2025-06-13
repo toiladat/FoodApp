c0727d64cff9ca0a560020430dbcb17d3bf7a39e
nguyen thanh thuy <nguyenthanhthuy@nguyens-MacBook-Air.local>
2025-06-13 08:04:49 +0700

    Khởi tạo trang cart

---
+++ app/src/main/java/com/example/foodapp/CartActivity.java
@@ -0,0 +1,17 @@
+package com.example.foodapp;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_cart);
+
+    }
+}
